6e3d474ff738faffa8f3259de346b76f39a5ecd9
flashslicer <michancheta@gmail.com>
2016-11-18 15:33:42 +0800

    Add files via upload

---
+++ TextAreaOutputStream.java
@@ -0,0 +1,21 @@
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.swing.JTextArea;
+
+  public  class TextAreaOutputStream extends OutputStream 
+    {
+        private JTextArea textControl;
+            
+        public TextAreaOutputStream(JTextArea control)
+        {
+            textControl=control;
+        }
+        
+        public void write(int b) throws IOException
+        {
+            textControl.append(String.valueOf((char)b));
+        }
+       
+    
+    }
